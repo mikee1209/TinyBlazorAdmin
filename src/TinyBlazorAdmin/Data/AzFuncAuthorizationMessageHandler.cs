17c10b2f7124686c0d8934708e5d703ebf38ee03
MichalStepien1 <238377@edu.p.lodz.pl>
2022-11-23 00:23:30 +0100

    handler update

--- src/TinyBlazorAdmin/Data/AzFuncAuthorizationMessageHandler.cs
+++ src/TinyBlazorAdmin/Data/AzFuncAuthorizationMessageHandler.cs
@@ -10,6 +10,7 @@ namespace TinyBlazorAdmin.Data
     public class AzFuncAuthorizationMessageHandler : AuthorizationMessageHandler
     {
         public string Endpoint { get; set; }
+        public string AppIdUri { get; set; }
 
         /// <summary>
         /// Creates a new instance of the <see cref="AzFuncAuthorizationMessageHandler"/>
